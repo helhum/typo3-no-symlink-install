4ef284c34e85f88b0cf265ed009c032f5481317f
Helmut Hummel <info@helhum.io>
2017-08-14 13:00:12 +0200

    Extract basic setup into helhum/typo3-composer-setup

--- src/Composer/InstallerScripts.php
+++ src/Composer/InstallerScripts.php
@@ -16,79 +16,29 @@ namespace Helhum\Typo3NoSymlinkInstall\Composer;
  */
 
 use Composer\Script\Event;
-use Composer\Semver\Constraint\EmptyConstraint;
-use Helhum\Typo3NoSymlinkInstall\Composer\InstallerScripts\EntryPoint;
-use Helhum\Typo3NoSymlinkInstall\Composer\InstallerScripts\Stop;
-use Helhum\Typo3NoSymlinkInstall\Composer\InstallerScripts\WebDirectory;
+use Helhum\Typo3ComposerSetup\Composer\InstallerScripts\RootDirectory;
+use TYPO3\CMS\Composer\Plugin\Config;
 use TYPO3\CMS\Composer\Plugin\Core\InstallerScriptsRegistration;
 use TYPO3\CMS\Composer\Plugin\Core\ScriptDispatcher;
 
 /**
- * Hook into Composer build to set up TYPO3 web directory if necessary
+ * Hook into Composer build to set up TYPO3 web directory with mirror strategy
  */
 class InstallerScripts implements InstallerScriptsRegistration
 {
-    private static $entryPoints = [
-        'frontend' => [
-            'target' => 'index.php',
-        ],
-        'backend' => [
-            'target' => 'typo3/index.php',
-        ],
-        'install' => [
-            'target' => 'typo3/install.php',
-        ],
-    ];
-
     /**
      * @param Event $event
      * @param ScriptDispatcher $scriptDispatcher
      */
     public static function register(Event $event, ScriptDispatcher $scriptDispatcher)
     {
-        $typo3CmsPackage = $event->getComposer()->getRepositoryManager()->getLocalRepository()->findPackage('typo3/cms', new EmptyConstraint());
-        $cmsInstallPath = $event->getComposer()->getInstallationManager()->getInstallPath($typo3CmsPackage);
-        self::determineEntryScriptSourceFiles($cmsInstallPath);
+        $composer = $event->getComposer();
+        $pluginConfig = Config::load($composer);
+        $webDir = $pluginConfig->get('web-dir');
 
         $scriptDispatcher->addInstallerScript(
-            new WebDirectory(),
-            80
+            new RootDirectory($webDir, RootDirectory::PUBLISH_STRATEGY_MIRROR),
+            75
         );
-        foreach (self::$entryPoints as $entryPoint) {
-            $scriptDispatcher->addInstallerScript(
-                new EntryPoint(
-                    $entryPoint['source'],
-                    $entryPoint['target']
-                ),
-                70
-            );
-
-        }
-        $scriptDispatcher->addInstallerScript(
-            new Stop(),
-            60
-        );
-    }
-
-    private static function determineEntryScriptSourceFiles(string $cmsInstallPath)
-    {
-        if (file_exists($frontendSourceFile = $cmsInstallPath . '/typo3/sysext/frontend/Resources/Private/Php/frontend.php')) {
-            self::$entryPoints['frontend']['source'] = $frontendSourceFile;
-        } else {
-            self::$entryPoints['frontend']['source'] = $cmsInstallPath . '/index.php';
-        }
-
-        if (file_exists($backendSourceFile = $cmsInstallPath . '/typo3/sysext/backend/Resources/Private/Php/backend.php')) {
-            self::$entryPoints['backend']['source'] = $backendSourceFile;
-        } else {
-            self::$entryPoints['backend']['source'] = $cmsInstallPath . '/typo3/index.php';
-        }
-
-        if (file_exists($installSourceFile = $cmsInstallPath . '/typo3/sysext/install/Resources/Private/Php/install.php')) {
-            self::$entryPoints['install']['source'] = $installSourceFile;
-        } else {
-            self::$entryPoints['install']['source'] = $cmsInstallPath . '/typo3/sysext/install/Start/Install.php';
-            self::$entryPoints['install']['target'] = 'typo3/sysext/install/Start/Install.php';
-        }
     }
 }
